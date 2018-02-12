f858af9f09dfbf76fc28400e06f02d6f4be20021
János Csjernyik <janos.csjernyik@gmail.com>
2018-02-12 10:18:03 +0100

    game state current_buy_in print

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print (game_state['current_buy_in'])
         return 1000
 
     def showdown(self, game_state):
