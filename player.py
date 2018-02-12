db92ec60806e60a335d78e9bfc3a4b13975ee9b1
Akos Putnoki <putnokiakos@gmail.com>
2018-02-12 11:44:06 +0100

    cefwe

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player:
     def betRequest(self, game_state):
         current_buyin = game_state["current_buy_in"]
         min_raise = game_state["minimum_raise"]
+        big_blind = game_state["small_blind"] * 2
         for player in game_state["players"]:
             if player["name"] == "Rafa El Diablo":
                 card1 = player["hole_cards"][0]["rank"]
@@ -15,6 +16,8 @@ class Player:
                     return current_buyin - player["bet"] + min_raise
                 elif (card1 in "AKQJ98" or card1 == "10") and (card2 in "AKQJ98" or card2 == "10"):
                     return current_buyin - player["bet"]
+                elif (current_buyin == big_blind):
+                    return current_buyin - player["bet"] + min_raise
         return 0
 
     def showdown(self, game_state):
