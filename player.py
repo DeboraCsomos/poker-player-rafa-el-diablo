a33f6bcf63c83b61b4a3d8d4be9e0623d0549cbf
Akos Putnoki <putnokiakos@gmail.com>
2018-02-12 12:12:28 +0100

    geriegoregj

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player:
         current_buyin = game_state["current_buy_in"]
         min_raise = game_state["minimum_raise"]
         big_blind = game_state["small_blind"] * 2
+        community_cards = game_state["community_cards"]
         for player in game_state["players"]:
             if player["name"] == "Rafa El Diablo":
                 card1 = player["hole_cards"][0]["rank"]
@@ -14,7 +15,11 @@ class Player:
                     if card1 in "AKQJ":
                         return current_buyin - player["bet"] + min_raise * 2
                     return current_buyin - player["bet"] + min_raise
-                elif (card1 in "AKQJ98" or card1 == "10") and (card2 in "AKQJ98" or card2 == "10"):
+                elif len(community_cards) > 0:
+                    for card in community_cards:
+                        if not (card["rank"] == card1 or card["rank"] == card2):
+                            return 0
+                if (card1 in "AKQJ98" or card1 == "10") and (card2 in "AKQJ98" or card2 == "10"):
                     return current_buyin - player["bet"]
                 elif (card1 == "A" and card2 in "12345") or (card2 == "A" and card1 in "12345"):
                     return current_buyin - player["bet"]
