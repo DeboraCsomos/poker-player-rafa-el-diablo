dd9a6f83b8737657f2bdc6b0f6930956b4353622
Akos Putnoki <putnokiakos@gmail.com>
2018-02-12 10:40:10 +0100

    login added

--- player.py
+++ player.py
@@ -1,10 +1,17 @@
 
 class Player:
-    VERSION = "hurka"
+    VERSION = "TOP STRATEGY RUSSIAN BOT"
 
     def betRequest(self, game_state):
-        print (game_state['players'])
-        return 1000
+        for player in game_state["players"]:
+            if player["name"] == "Rafa El Diablo":
+                card1 = player["hole_cards"][0]["rank"]
+                card2 = player["hole_cards"][1]["rank"]
+                if card1 == card2:
+                    return 1000
+                elif card1 in "AKQJ" or card2 == "AKQJ":
+                    return 500
+        return 400
 
     def showdown(self, game_state):
         pass
